13beeb1e91335c433068d9ad36b271d4146012b7
Varun Gungi <54449583+meenank@users.noreply.github.com>
2020-11-04 19:15:42 +0530

    js files

---
+++ project/js/validate.js
@@ -0,0 +1,25 @@
+function ValidateInput(){
+  var name1 = document.forms["sayhello"]["name"].value;
+  var mobile1 = document.forms["sayhello"]["phone"].value;
+  var mailid1 = document.forms["sayhello"]["mail"].value;
+
+  if(name1==""){
+    window.alert("Please fill your name!");
+      document.getElementById("cusname").focus();
+      document.getElementById("cusname").style.border = "2px solid red"; q
+  }
+
+  if(mobile1==""){
+    window.alert("Please fill your mobile number!");
+    document.getElementById("cusphone").focus();
+  }
+
+  if(mailid1==""){
+    window.alert("Please fill your mail-id!");
+      document.getElementById("cusmail").focus();
+  }
+
+  if(name1!="" && mobile1!="" && mailid1!=""){
+    document.getElementById("formhead").innerHTML="data submitted, will respond in 24hrs :)"
+  }
+}
